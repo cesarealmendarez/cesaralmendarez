137e2e6561f2f5e86f96a28b58daf0f70e1245de
Cesar Almendarez <89669770+cesarealmendarez@users.noreply.github.com>
2023-03-09 22:30:23 -0800

    ❌ Troubleshooting Router

--- src/index.js
+++ src/index.js
@@ -2,20 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { NextUIProvider } from '@nextui-org/react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
     <NextUIProvider>
-        {/* <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/*" element={<App/>}/>
             </Routes>
-        </BrowserRouter> */}
-        <div>
-            <h1>Troubleshooting Routing</h1>
-        </div>
+        </HashRouter>
     </NextUIProvider>
 )
